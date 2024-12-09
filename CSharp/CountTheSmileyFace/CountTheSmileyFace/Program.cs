96b269ddc5d07e86737cb57b080eeed4b4be9394
Nuss Eduard Andreevich <nuss.ea@arkan.ru>
2024-12-09 15:43:55 +0300

    6 kyu - Count The Smiley Faces

---
+++ CSharp/CountTheSmileyFace/CountTheSmileyFace/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace CountTheSmileyFace
+{
+   internal class Program
+   {
+      static void Main(string[] args) {
+         var smile = new string[] { ":D", ":~)", ";~D", ":)" };
+         Console.WriteLine(CountSmileys(smile));
+         Console.ReadKey();
+      }
+      /// <summary>
+      /// 6 kyu - Count the smiley faces! - https://www.codewars.com/kata/583203e6eb35d7980400002a/train/csharp - DONE
+      /// </summary>
+      /// <param name="smileys"></param>
+      /// <returns></returns>
+      public static int CountSmileys(string[] smileys) {
+         if (smileys is null)
+            return 0;
+
+         string pattern = @"^[:;][-~]?[)D]$";
+         return smileys.Count(face => Regex.IsMatch(face, pattern));
+      }
+   }
+}
